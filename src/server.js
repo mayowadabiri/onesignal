ec7460116aea1aa6fa1b490d4ef18a76f67187bd
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2022-07-06 16:43:11 +0100

    chore:setting up the project structure

---
+++ src/server.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+
+// UNCAUGHT EXCEPTIONS
+// Application needs to be crashed then a tool will be needed to restart the APP
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION!...");
+  console.log({ err });
+  console.log(err.name, err.message);
+  process.exit();
+});
+
+const app = require("./app");
+
+// Database
+
+//   START SERVER
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (!Number.isNaN(port)) {
+    return val;
+  }
+
+  if (port > 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || "8888");
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+// create a http server
+const server = app.listen(port, async () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? `pipe ${address}` : `port ${port}`;
+  console.log(`Listening on ${bind}`);
+});
+
+// Catching Exceptions
+
+// Application does not necessarily need to be crashed
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION!...");
+  console.log(err.name, err.message);
+  console.log({ err });
+  server.close(() => {
+    process.exit();
+  });
+});
+
+// process.on("SIGINT", async () => {
+//   await mongoose.connection.close();
+//   process.exit(0);
+// });
